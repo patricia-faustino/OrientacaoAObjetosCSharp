0fb370db95ea209402c199db191405d600b23ee9
Patricia Faustino de Brito de Jesus <patricia.faustino@iteris.com.br>
2021-06-07 23:12:10 -0300

    Implementando metodos DefinirSaldo e ObterSaldo

--- 01-ByteBank/ContaCorrente.cs
+++ 01-ByteBank/ContaCorrente.cs
@@ -6,33 +6,47 @@ namespace _01_ByteBank
         public Cliente titular;
         public int agencia;
         public int numero;
-        public double saldo = 100;
+        private double saldo = 100;
+
+        public double ObterSaldo()
+        {
+            return saldo;
+        }
+
+        public void DefinirSaldo(double saldo)
+        {
+          if(saldo >= 0) 
+            {
+                this.saldo = saldo;
+            }
+            return;
+        }
 
         public bool Sacar(double valor)
         {
-            if (this.saldo < valor)
+            if (saldo < valor)
             {
                 return false;
             }
 
-            this.saldo -= valor;
+            saldo -= valor;
             return true;
 
         }
 
         public void Depositar(double valor)
         {
-            this.saldo += valor;
+            saldo += valor;
         }
 
         public bool Transferir(double valor, ContaCorrente contaDestino)
         {
-            if (this.saldo < valor)
+            if (saldo < valor)
             {
                 return false;
             }
 
-            this.saldo -= valor;
+            saldo -= valor;
             contaDestino.Depositar(valor);
             return true;
         }
--- 01-ByteBank/Program.cs
+++ 01-ByteBank/Program.cs
@@ -10,32 +10,11 @@ namespace _01_ByteBank
     {
         static void Main(string[] args)
         {
-            /**Cliente gabriela = new Cliente();
-            gabriela.nome = "Gabriela";
-            gabriela.cpf = "434.562.878-20";
-            gabriela.profissao = "Desenvolvedora C#";*/
-
             ContaCorrente conta = new ContaCorrente();
-            //conta.titular = new Cliente();
-            //conta.titular.nome = "Gabriela Costa";
-            //conta.titular.cpf = "434.562.878-20";
-            //conta.titular.profissao = "Desenvolvedora C#";
-            conta.saldo = 500;
-            conta.agencia = 563;
-            conta.numero = 5634527;
+            conta.DefinirSaldo(-10);
 
-            if (conta.titular == null)
-            {
-                Console.WriteLine("Ops. A referência está apontando para NULL.");
-            }
+            Console.WriteLine(conta.ObterSaldo());
 
-
-            Console.WriteLine(conta.titular);
-
-            //Console.WriteLine(gabriela.nome);
-           // Console.WriteLine(conta.titular.nome);
-           //Console.WriteLine(conta.titular.cpf);
-           // Console.WriteLine(conta.titular.profissao);
 
             Console.ReadLine();
         }
